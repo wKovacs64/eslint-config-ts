144277157847216bd52be032056f6f9b29438baa
Justin Hall <justin.r.hall@gmail.com>
2019-02-06 16:32:29 -0700

    feat: initial release

---
+++ README.md
@@ -0,0 +1,29 @@
+# @wkovacs64/eslint-config-ts
+
+This is my base personal ESLint configuration for TypeScript projects.
+
+### Install
+
+```
+npx install-peerdeps --dev --yarn @wkovacs64/eslint-config-ts
+```
+
+> Drop the `--yarn` when using `npm`.
+
+### Usage
+
+Extend in your `.eslintrc.js`:
+
+```js
+module.exports = {
+  extends: ['@wkovacs64/eslint-config-ts'],
+};
+```
+
+### Other Configurations
+
+- Base: [@wkovacs64/eslint-config](https://github.com/wkovacs64/eslint-config)
+- React:
+  [@wkovacs64/eslint-config-react](https://github.com/wkovacs64/eslint-config-react)
+- TypeScript and React:
+  [@wkovacs64/eslint-config-ts-react](https://github.com/eslint-config-ts-react)
---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = {
+  extends: ['@wkovacs64/eslint-config', 'prettier/@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/class-name-casing': 'error',
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      { allowExpressions: true },
+    ],
+    '@typescript-eslint/interface-name-prefix': 'error',
+    '@typescript-eslint/no-angle-bracket-type-assertion': 'error',
+    '@typescript-eslint/no-empty-interface': 'error',
+    '@typescript-eslint/no-inferrable-types': [
+      'error',
+      { ignoreProperties: false, ignoreParameters: false },
+    ],
+    '@typescript-eslint/no-namespace': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/no-object-literal-type-assertion': 'error',
+    '@typescript-eslint/no-parameter-properties': 'error',
+    '@typescript-eslint/no-triple-slash-reference': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/prefer-interface': 'error',
+    '@typescript-eslint/prefer-namespace-keyword': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts', '.tsx'],
+      },
+    },
+  },
+};
